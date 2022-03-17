aa3741a5f9bfb9f4ea1350e009b48f796ff60809
Collin Brooks <cbrooks@sentryds.com>
2022-03-16 22:10:10 -0500

    Fix LoadConfig constructor with RawData

--- src/Bundle/ApiServicesBundle/Models/Loader/Config/LoadConfig.php
+++ src/Bundle/ApiServicesBundle/Models/Loader/Config/LoadConfig.php
@@ -15,7 +15,8 @@ class LoadConfig
         $parent = null,
         bool $clearCache = null,
         ExceptionHandlerInterface $handler = null,
-        array $existingData = null
+        array $existingData = null,
+        $rawData = null
     ) {
         $this->client = $client;
         $this->commandArgs = $commandArgs ?? [];
@@ -23,6 +24,7 @@ class LoadConfig
         $this->clearCache = $clearCache ?? false;
         $this->handler = $handler;
         $this->existingData = $existingData;
+        $this->rawData = $rawData;
     }
 
     public static function builder(string $forClass, ServiceClientInterface $client): LoadConfigBuilder
